7848d3c17ea1655f0462502b346d5e5f1e57163e
Christophe Querton <kertof@gmail.com>
2018-12-13 15:48:54 +0100

    adds service worker

--- uuidgen/src/app/app.module.ts
+++ uuidgen/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UuidComponent } from './uuid/uuid/uuid.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, UuidComponent],
@@ -26,6 +28,9 @@ import { UuidComponent } from './uuid/uuid/uuid.component';
     MatCardModule,
     HttpClientModule,
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
