b6c5d8e104adc0a38567d791b6498e3250454f66
Christophe Querton <kertof@gmail.com>
2018-12-14 18:15:52 +0100

    cleanup.

--- uuidgen/src/app/app.module.ts
+++ uuidgen/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {
   MatButtonModule,
-  MatCheckboxModule,
   MatToolbarModule,
   MatIconModule,
   MatCardModule,
