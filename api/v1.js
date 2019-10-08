174e7135382f8970195f8ded096e61986574b95e
Christophe Querton <kertof@gmail.com>
2019-10-08 15:59:24 +0200

    adds api scripts.

---
+++ api/v1.js
@@ -0,0 +1,42 @@
+const uuidv1 = require('uuid/v1');
+
+const singleUuid = () => {
+  return (req, res) => {
+    const uuid = uuidv1();
+    const statusText = 'OK';
+    res.status(200).json({ uuid, statusText });
+  };
+};
+
+const multiUuid = num => {
+  return (req, res) => {
+    let uuids = [];
+    statusText = 'OK';
+    // Limit to 10;
+    if (num > 10) {
+      num = 10;
+      statusText = 'OK. 10 UUIDs max.';
+    }
+    for (let i = 0; i <= num; i++) {
+      uuids.push(uuidv1());
+    }
+    status = 200;
+
+    res.status(status).json({ uuids, statusText });
+  };
+};
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+module.exports = (req, res) => {
+  let num = +req.query.number;
+  if (num) {
+    multiUuid(num);
+  } else {
+    singleUuid();
+  }
+};
