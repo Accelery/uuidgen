2afc8ca927cfc0082cc6547fb75e7c8af9aaf45a
Christophe Querton <kertof@gmail.com>
2020-01-11 17:52:37 +0100

    back to now

--- api/v1.js
+++ api/v1.js
@@ -1,14 +1,13 @@
-('use strict');
 const uuidFn = require('uuid/v1');
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
+
 const apiHandler = require('./_apiHandler');
 
-app.get('/', (res, req) => {
-  let number = req.query.number;
-  let result = apiHandler(uuidFn, number);
-  res.json(result);
-});
-
-module.exports.handler = serverless(app);
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+module.exports = (req, res) => {
+  apiHandler(uuidFn, req, res);
+};
