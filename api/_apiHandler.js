24b604943cded28fb22bd01f2fc5751a7e2a02de
Christophe Querton <kertof@gmail.com>
2020-01-15 12:59:05 +0100

    fix endpojnt

--- api/_apiHandler.js
+++ api/_apiHandler.js
@@ -1,14 +1,14 @@
-module.exports = (uuidFn, number) => {
-  if (number) {
+module.exports = (uuidFn, req, res) => {
+  if (req.query.number) {
     // Limit to 10;
-    const num = Math.min(+number, 10);
+    const num = Math.min(+req.query.number, 10);
     let uuids = [];
     for (let i = 0; i <= num; i++) {
       uuids.push(uuidFn());
     }
-    return { uuids };
+    res.json({ uuids });
   } else {
     const uuid = uuidFn();
-    return { uuid };
+    res.json({ uuid });
   }
 };
