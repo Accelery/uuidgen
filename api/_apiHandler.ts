f11576c636b727cc37e582ff39d46e0661e8d086
Christophe Querton <kertof@gmail.com>
2020-01-20 22:55:27 +0100

    migrate to TS

--- api/_apiHandler.ts
+++ api/_apiHandler.ts
@@ -1,14 +1,14 @@
-module.exports = (uuidFn, req, res) => {
+export const apiHelper = (uuidFn, req) => {
   if (req.query.number) {
     // Limit to 10;
     const num = Math.min(+req.query.number, 10);
-    let uuids = [];
+    const uuids = [];
     for (let i = 0; i <= num; i++) {
       uuids.push(uuidFn());
     }
-    res.json({ uuids });
+    return { uuids };
   } else {
     const uuid = uuidFn();
-    res.json({ uuid });
+    return { uuid };
   }
 };
