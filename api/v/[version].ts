417ff741d441cd400a0d9d8a912004fe02553a0c
Christophe Querton <kertof@gmail.com>
2020-01-29 11:05:42 +0100

    simpler

--- api/v/[version].ts
+++ api/v/[version].ts
@@ -5,18 +5,7 @@ import { v1, v4 } from 'uuid';
  * Responds to any HTTP request.
  */
 export default async (req: NowRequest, res: NowResponse) => {
-  let uuidFn;
-  console.log(+req.query.version);
-  switch (+req.query.version) {
-    case 1:
-      uuidFn = v1;
-      break;
-    case 4:
-      uuidFn = v4;
-      break;
-    default:
-      uuidFn = v4;
-  }
+  const uuidFn = +req.query.version === 1 ? v1 : v4;
   const num = req.query.q ? Math.min(+req.query.q, 10) : 1;
   const uuids = [];
   for (let i = 0; i < num; i++) {
