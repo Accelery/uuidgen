7bf1ce9b7dfa5daba6c048fdd9ccf4e7347030ec
Christophe Querton <kertof@gmail.com>
2020-01-29 10:33:39 +0100

    testing

--- api/v/[version].ts
+++ api/v/[version].ts
@@ -1,10 +1,19 @@
 import { NowRequest, NowResponse } from '@now/node';
+import { v1, v4 } from 'uuid';
 
 /**
  * Responds to any HTTP request.
  */
-export default async (req: NowRequest, response: NowResponse) => {
-  const uuidFn = await import('uuid/' + req.query.version);
+export default async (req: NowRequest) => {
+  let uuidFn;
+  switch (+req.query.version) {
+    case 1:
+      uuidFn = v1;
+      break;
+    case 4:
+      uuidFn = v4;
+      break;
+  }
   const num = req.query.q ? Math.min(+req.query.q, 10) : 1;
   const uuids = [];
   for (let i = 0; i < num; i++) {
