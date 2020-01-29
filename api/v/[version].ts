030d4152ee212fe1b178b8655c52e5f9f3fdef99
Christophe Querton <kertof@users.noreply.github.com>
2020-01-29 15:50:17 +0100

    Simplify API code. (#3)

---
+++ api/v/[version].ts
@@ -0,0 +1,15 @@
+import { NowRequest, NowResponse } from '@now/node';
+import { v1, v4 } from 'uuid';
+
+/**
+ * Responds to any HTTP request.
+ */
+export default async (req: NowRequest, res: NowResponse) => {
+  const uuidFn = +req.query.version === 1 ? (v1 as Function) : (v4 as Function);
+  const num = req.query.q ? Math.min(+req.query.q, 10) : 1;
+  const uuids = [];
+  for (let i = 0; i < num; i++) {
+    uuids.push(uuidFn());
+  }
+  res.json(uuids);
+};
